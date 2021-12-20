b6736f7575b43cd21ef93ee5ed44fe74a2e6dd71
Aidar Kazymbaev <kazymbaevaidar@gmail.com>
2021-12-20 17:01:33 +0600

    1

--- src/helpers/const.js
+++ src/helpers/const.js
@@ -1,4 +1,4 @@
 // export const API = "https://projectaidar1.herokuapp.com/api/products";
-export const API = "https://aidarsflower.herokuapp.com";
+export const API = "https://aidarsflower.herokuapp.com/products";
 export const APIusers = "https://aidarsflower.herokuapp.com/users";
 export const COMMENT_API = "https://aidarsflower.herokuapp.com/comments";
